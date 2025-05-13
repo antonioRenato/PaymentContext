deef12b2b23be80c43a74b0d3f231c301b40fd9c
antonioRenato <antoniorpfs@gmail.com>
2025-05-13 20:48:01 -0300

    Add project files.

---
+++ src/PaymentContext.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PaymentContext.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/PaymentContext.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PaymentContext.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
