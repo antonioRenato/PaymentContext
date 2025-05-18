dd2770a7d495701d64ebeffdc3bdcf11bf60cd7a
antonioRenato <antoniorpfs@gmail.com>
2025-05-18 10:56:12 -0300

    Created Entities

---
+++ src/PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        public string Number { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+    }
+}
--- src/PaymentContext.Domain/Entities/Student.cs
+++ src/PaymentContext.Domain/Entities/Student.cs
@@ -6,5 +6,6 @@
         public string LastName { get; set; }
         public string Document {  get; set; }
         public string Email { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
     }
 }
