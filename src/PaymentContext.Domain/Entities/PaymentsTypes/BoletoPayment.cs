c33ea0486d14953e234984f887f19e9d26495a40
antonioRenato <antoniorpfs@gmail.com>
2025-06-01 10:42:37 -0300

    Fixed all entities do open/closed principal

--- src/PaymentContext.Domain/Entities/PaymentsTypes/BoletoPayment.cs
+++ src/PaymentContext.Domain/Entities/PaymentsTypes/BoletoPayment.cs
@@ -2,7 +2,20 @@
 {
     public class BoletoPayment : Payment
     {
-        public string BarCode { get; set; }
-        public string  BoletoNumber { get; set; }
+        public BoletoPayment(string barCode, string boletoNumber, DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string document,
+            string payer,
+            string address,
+            string email) : base(paidDate, expireDate, total, totalPaid, document, payer, address, email)
+        {
+            BarCode = barCode;
+            BoletoNumber = boletoNumber;
+        }
+
+        public string BarCode { get; private set; }
+        public string  BoletoNumber { get; private set; }
     }
 }
--- tests/PaymentContext.Tests/Entities/StudentTests.cs
+++ tests/PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,14 +1,18 @@
-﻿using PaymentContext.Domain.Entities;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
 
 namespace PaymentContext.Tests.Entities
 {
+    [TestClass]
     public class StudentTests
     {
-        [Fact]
+        [TestMethod]
         public void Test1()
         {
+            var subscriptions = new Subscription(null);
             var student = new Student("André", "teste", "231313", "antonio@gmail.com");
 
+            student.AddSubscription(subscriptions);
         }
     }
 }
