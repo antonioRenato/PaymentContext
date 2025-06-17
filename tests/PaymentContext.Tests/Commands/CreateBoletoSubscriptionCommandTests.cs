45853b5c3983a0070ca747dda4d01a05c2cc21fd
antonioRenato <antoniorpfs@gmail.com>
2025-06-17 06:52:49 -0300

    Created test for command

---
+++ tests/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [Fact]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+            command.LastName = "Silva";
+
+            command.Validate();
+            Assert.False(command.Valid);
+        }
+    }
+}
