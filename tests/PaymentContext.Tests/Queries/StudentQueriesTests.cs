6640713ce5c0e17f9563ee8eb0e5e0544bbc4c92
antonioRenato <antoniorpfs@gmail.com>
2025-07-01 07:26:12 -0300

    created tests for query

---
+++ tests/PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,44 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    public class StudentQueriesTests
+    {
+        // Red, Green, Refactor
+        private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            _students = new List<Student>();
+            for (var i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("1111111111" + i.ToString(), EDocumentType.CPF),
+                    new Email(i.ToString() + "@balta.io")
+                ));
+            }
+        }
+
+        [Fact]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudent("12345678911");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.Null(studn);
+        }
+
+        [Fact]
+        public void ShouldReturnStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudent("11111111111");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.NotNull(studn);
+        }
+    }
+}
